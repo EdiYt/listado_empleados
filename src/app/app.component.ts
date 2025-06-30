6df695345549bf2659b168973a821bd49e55f920
Erick <erickvillaseor@yahoo.com.mx>
2025-06-29 22:35:26 -0600

    Cree el css para la lista de empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { FormularioEmpleadoComponent } from './components/formulario-empleado/formulario-empleado.component';
 import { ListaEmpleadoComponent } from './components/lista-empleado/lista-empleado.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     RouterOutlet,
-    FormsModule, 
     FormularioEmpleadoComponent,
     ListaEmpleadoComponent
   ],
