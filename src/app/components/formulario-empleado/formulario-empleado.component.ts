9c8f7dd7f6e78f78b8bb35715c3d4a2da2c73787
Erick <erickvillaseor@yahoo.com.mx>
2025-06-29 22:06:17 -0600

    Se termino el .ts del formulario empleado creo

--- src/app/components/formulario-empleado/formulario-empleado.component.ts
+++ src/app/components/formulario-empleado/formulario-empleado.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Employee } from '../../models/empleados';
+import { EmpleadoService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-formulario-empleado',
-  imports: [],
   templateUrl: './formulario-empleado.component.html',
-  styleUrl: './formulario-empleado.component.css'
+  styleUrls: ['./formulario-empleado.component.css']
 })
 export class FormularioEmpleadoComponent {
+  empleado: Employee = {
+    id: 0,
+    nombre: '',
+    email: '',
+    telefono: '',
+    fechaNacimiento: '',
+    genero: ''
+  };
 
+  @Output() empleadoAgregado = new EventEmitter<void>();
+
+  constructor(private empleadoService: EmpleadoService) {}
+
+  onSubmit(): void {
+    this.empleado.id = this.empleadoService.obtenerSiguienteId();
+    this.empleadoService.agregarEmpleado(this.empleado);
+    this.empleadoAgregado.emit();
+    this.resetForm();
+  }
+
+  resetForm(): void {
+    this.empleado = {
+      id: 0,
+      nombre: '',
+      email: '',
+      telefono: '',
+      fechaNacimiento: '',
+      genero: ''
+    };
+  }
 }
