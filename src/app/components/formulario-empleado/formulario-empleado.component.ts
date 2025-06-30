78cbe63944a47dfe2f2aa5818fd6ebfb54abdf70
Erick <erickvillaseor@yahoo.com.mx>
2025-06-29 21:44:56 -0600

    Se creo el componente del formulario

---
+++ src/app/components/formulario-empleado/formulario-empleado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-empleado',
+  imports: [],
+  templateUrl: './formulario-empleado.component.html',
+  styleUrl: './formulario-empleado.component.css'
+})
+export class FormularioEmpleadoComponent {
+
+}
