571e8b424ad0b585ef56cffc20ce6b83b7c98712
Erick <erickvillaseor@yahoo.com.mx>
2025-06-29 21:48:03 -0600

    Se creo el componente de la lista de empleados

---
+++ src/app/components/lista-empleado/lista-empleado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-empleado',
+  imports: [],
+  templateUrl: './lista-empleado.component.html',
+  styleUrl: './lista-empleado.component.css'
+})
+export class ListaEmpleadoComponent {
+
+}
